955ba318218f385e073f7549ff78bc53fda87c8f
Dmytro Ivashchuk <dimaiv@me.com>
2019-12-06 22:34:52 +0100

    fix body styles

--- src/blog/components/body.js
+++ src/blog/components/body.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import Sidebar from "./sidebar.js";
 import Suggested from "./suggested.js";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
@@ -6,12 +7,22 @@ import "tachyons";
 import "../../common/styles/custom.tachyons.css";
 import "../styles/grid.css";
 
+const StyledBody = styled.div`
+  @media (max-width: 600px) {
+    font-size: 1rem;
+    padding: 0 25px;
+  }
+`;
+
 export default props => (
   <div className="min-vh-100 blog__grid">
     <div style={{ gridArea: "header" }} />
-    <div className="mw8 serif f4 lh-copy center pa2 article__container">
+    <StyledBody
+      style={{ gridArea: "content", textAlign: "justify" }}
+      className="mw8 serif f4 lh-copy center pa2 article__container"
+    >
       {documentToReactComponents(props.content)}
-    </div>
+    </StyledBody>
 
     <Sidebar
       img={props.image}
