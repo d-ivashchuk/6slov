f13afea7abca2e9feab2f272d945cb15aac8276d
Dmytro Ivashchuk <dimaiv@me.com>
2019-12-06 22:11:49 +0100

    remove feed from the project

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,61 +27,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
-    {
-      resolve: "gatsby-plugin-feed",
-      options: {
-        query: `
-        {
-          site {
-            siteMetadata {
-              title
-              description
-              siteUrl
-              site_url: siteUrl
-            }
-          }
-        }
-      `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map(edge => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
-                  custom_elements: [{ "content:encoded": edge.node.html }]
-                });
-              });
-            },
-            query: `
-            {
-              allMarkdownRemark(
-                limit: 1000,
-                sort: { order: DESC, fields: [frontmatter___date] },
-                filter: {frontmatter: {type: {eq: "post"}}}
-              ) {
-                edges {
-                  node {
-                    excerpt
-                    html
-                    frontmatter {
-                      slug
-                      title
-                      date
-                    }
-                  }
-                }
-              }
-            }
-          `,
-            output: "/rss.xml",
-            title: "Gatsby RSS Feed"
-          }
-        ]
-      }
-    },
+
     {
       resolve: "gatsby-source-filesystem",
       options: {
